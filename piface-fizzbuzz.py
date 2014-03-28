618c37283b0c3405809ae2ac80f773b54c7d99be
Phil T <dibblethewrecker@gmail.com>
2014-03-28 16:11:01 +0000

    Turn the script into a function

--- piface-fizzbuzz.py
+++ piface-fizzbuzz.py
@@ -1,23 +1,28 @@
 #!/bin/env/python
-# start with a counter i
-i=1
-# loop through i 1 to 20
-while i<20:
+
+# define a function that checks for Fizzbuzz
+def checkFizzbuzz(i):
     # if i can be divided exactly by 5
     if (i % 5 == 0):
         # can it also be divided by 2
         if (i % 2 == 0):
             # Fizzbuzz
-            print("Fizzbuzz")
+            return("Fizzbuzz")
         else:
             # if not then just Buzz
-            print("Buzz")
+            return("Buzz")
     # can i be divided by 2
     elif (i % 2 == 0):
         # then Fizz
-        print("Fizz")
+        return("Fizz")
     else:
-        #otherwise just print i
-        print(i)
+        #otherwise just return i
+        return(i)
+
+# start with a counter i
+i=1
+# loop through i 1 to 20
+while i<20:
+    print(checkFizzbuzz(i))
     # increment the counter
     i += 1
