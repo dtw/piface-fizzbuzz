510bd6769d04ad6eb15568fb0607a59a09fb4f99
Phil T <dibblethewrecker@gmail.com>
2014-03-28 16:12:21 +0000

    Introduce player interaction

--- piface-fizzbuzz.py
+++ piface-fizzbuzz.py
@@ -17,12 +17,29 @@ def checkFizzbuzz(i):
         return("Fizz")
     else:
         #otherwise just return i
-        return(i)
+        return(str(i))
+
+# create an array of player names
+player_names = [""]
+# get a name for Player 1
+player_names[0] = raw_input("Hi, what's your name Player " + str(len(player_names)) + "? ")
 
 # start with a counter i
 i=1
 # loop through i 1 to 20
 while i<20:
-    print(checkFizzbuzz(i))
+    # check who's turn it is - 3 players take turns, 2 computer players, computer starts
+    if (i % 3 == 0):
+        # it's Player 1's turn so get an answer and check if it is correct
+        # we use lower to make sure case matches
+        if raw_input(player_names[0] + " play! ").lower() != checkFizzbuzz(i).lower():
+            # if wrong
+            print("Wrong! You lose!")
+            break
+        else:
+            # if correct
+            print(checkFizzbuzz(i))
+    else:   
+        print(checkFizzbuzz(i))
     # increment the counter
     i += 1
