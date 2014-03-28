73272442752c01ea88cabf5d975d35cdb36f3c48
Phil T <dibblethewrecker@gmail.com>
2014-03-28 16:26:24 +0000

    ...too many changes for one commit. Get cpu player names from a file.

--- piface-fizzbuzz.py
+++ piface-fizzbuzz.py
@@ -90,7 +90,10 @@ def randomiseArray(start_array,no_samples=0):
 lightsOut()
 
 # create a list of cpu names
-cpu_names = ["HAL","WOPR","2501","Jarvis","GERTY"]
+file=open("/home/pi/cpu_names.txt",'r')
+cpu_names=file.read().splitlines()
+#cpu_names=file.readline().split()
+#cpu_names = ["HAL","WOPR","2501","Jarvis","GERTY"]
 
 # initialise a list of players
 player_names = []
